7900f9f4a3940020aaff77bc9224ffe1542f1737
und3f1n3d5 <55402266+und3f1n3d5@users.noreply.github.com>
2020-11-21 21:29:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,26 +61,28 @@ class BotHandler:
         try:
             self.users[str(user_id)].add_event(update)
             self.send_message(user_id, "Добавлено")
-        except:
+        except Exception as e:
+            print(e, "in add events, message:", update)
             self.send_message(user_id, "Некорректный формат")
 
     def remove_events(self, user_id, remove):
         try:
             self.users[str(user_id)].remove_event(remove)
             self.send_message(user_id, "Удалено")
-        except:
+        except Exception as e:
+            print(e, "in remove events, message:", remove)
             self.send_message(user_id, "Некорректный формат или нет такого события")
 
     def start_update(self, user_id):
         self.send_message(user_id, "Введите событие, которое хотите добавить в формате как в примере:\n\t Thu 12:20 "
-                                   "<Кр по ОКТЧ>\n После того как добавите все события (каждое в новом сообщении) "
-                                   "введите команду /stop_adding, чтобы сохранить изменения")
+                                   "<Кр по ОКТЧ>\n (<> - обязательны!)\nПосле того как удалите все ненужные события ("
+                                   "каждое в новом сообщении) введите команду /stop_adding, чтобы сохранить изменения")
         self.users[user_id].is_updating = True
 
     def start_removing(self, user_id):
         self.send_message(user_id, "Введите событие, которое хотите удалить в формате как в примере:\n\t Thu 12:20 "
-                                   "<Кр по ОКТЧ>\n После того как удалите все ненужные события (каждое в новом "
-                                   "сообщении) введите команду /stop_removing, чтобы сохранить изменения")
+                                   "<Кр по ОКТЧ>\n (<> - обязательны!)\nПосле того как удалите все ненужные события ("
+                                   "каждое в новом сообщении) введите команду /stop_removing, чтобы сохранить изменения")
         self.users[user_id].is_removing = True
 
     def end_update(self, user_id):
@@ -106,17 +108,24 @@ class BotHandler:
         self.users_write = open("users.txt", "w")
         for user in self.users:
             self.users[user].write(self.users_write)
+        self.users_write.close()
 
     def show_timetable(self, user_id):
         ans = ""
         for event in self.users[user_id].events:
-            ans += event.day + " " + event.hour + ":" + event.minute + " " + event.message + "\n";
+            ans += event.day + " " + str(event.hour) + ":" + str(event.minute) + " " + event.message + "\n"
+        if not ans:
+            ans = "Кажется у вас нет ни одного события!"
         self.send_message(user_id, ans)
 
     def start_message(self, chat_id):
         self.update_users(chat_id)
         self.send_message(chat_id, 'Приветствую, друг! Я бот, который может напоминать тебе о событиях. Нажми '
                                    '/help, чтобы увидеть мои команды')
+
+    def refresh(self):
+        for user in self.users:
+            self.users[user].refresh()
 
 
 class User:
@@ -132,7 +141,11 @@ class User:
 
     def remove_event(self, string):
         event = Event(string)
-        self.events.remove(event)
+        to_rm = event
+        for e in self.events:
+            if e.day == event.day and e.minute == event.minute and e.message == event.message:
+                to_rm = e
+        self.events.remove(to_rm)
 
     def get_next_event(self):
         var = time.strftime("%a#%H^%M:%S", time.localtime())
@@ -149,7 +162,11 @@ class User:
         file.write("#\n")
         file.write(self.id + "\n")
         for event in self.events:
-            file.write(event.day + " " + event.hour + ":" + event.minute + " <" + event.message + ">\n")
+            file.write(event.day + " " + str(event.hour) + ":" + str(event.minute) + " <" + event.message + ">\n")
+
+    def refresh(self):
+        for event in self.events:
+            event.reminded = False
 
 
 class Event:
@@ -164,50 +181,70 @@ class Event:
 time_bot = BotHandler(token)
 bot = telebot.TeleBot('1411772657:AAFkziVjMcehzkWDRWJyrnt7au7EBqDL9nQ')
 
+
 def main():
     new_offset = None
 
     while True:
-        upds = bot.get_updates(offset=new_offset)
-        if upds:
-            last_update = upds[-1]
-            last_update_id = last_update.update_id
-            last_chat_text = last_update.message.text
-            last_chat_id = str(last_update.message.chat.id)
-            time_bot.update_users(last_chat_id)
+        try:
+            upds = bot.get_updates(offset=new_offset)
+            if upds:
+                last_update = upds[-1]
+                last_update_id = last_update.update_id
+                last_chat_text = last_update.message.text
+                last_chat_id = str(last_update.message.chat.id)
+                time_bot.update_users(last_chat_id)
 
-            if last_chat_text.find("/start") != -1:
-                time_bot.start_message(last_chat_id)
+                in_process = time_bot.users[last_chat_id].is_removing or time_bot.users[last_chat_id].is_updating
 
-            if last_chat_text.find("/help") != -1:
-                time_bot.help(last_chat_id)
+                if last_chat_text.find("/start") != -1 and not in_process:
+                    time_bot.start_message(last_chat_id)
+                elif in_process and last_chat_text.find("/start") != -1:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс добавления/удаления")
 
-            if last_chat_text.find("/reset") != -1:
-                time_bot.reset(last_chat_id)
+                if last_chat_text.find("/help") != -1 and not in_process:
+                    time_bot.help(last_chat_id)
+                elif in_process and last_chat_text.find("/help") != -1:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс добавления/удаления")
 
-            if last_chat_text.find("/show_timetable") != -1:
-                time_bot.show_timetable(last_chat_id)
+                if last_chat_text.find("/reset") != -1 and not in_process:
+                    time_bot.reset(last_chat_id)
+                elif in_process and last_chat_text.find("/reset") != -1:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс добавления/удаления")
 
-            if last_chat_text.find("/add_events") != -1:
-                time_bot.start_update(last_chat_id)
-            elif last_chat_text.find("/stop_adding") != -1:
-                time_bot.end_update(last_chat_id)
-            elif time_bot.users[last_chat_id].is_updating:
-                time_bot.add_events(last_chat_id, last_chat_text)
+                if last_chat_text.find("/show_timetable") != -1 and not in_process:
+                    time_bot.show_timetable(last_chat_id)
+                elif in_process and last_chat_text.find("/show_timetable") != -1:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс добавления/удаления")
 
-            if last_chat_text.find("/remove_events") != -1:
-                time_bot.start_removing(last_chat_id)
-            elif last_chat_text.find("/stop_removing") != -1:
-                time_bot.end_removing(last_chat_id)
-            elif time_bot.users[last_chat_id].is_removing:
-                time_bot.remove_events(last_chat_id, last_chat_text)
+                if last_chat_text.find("/add_events") != -1 and not in_process:
+                    time_bot.start_update(last_chat_id)
+                elif last_chat_text.find("/add_events") != -1 and time_bot.users[last_chat_id].is_removing:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс удаления")
+                elif last_chat_text.find("/stop_adding") != -1:
+                    time_bot.end_update(last_chat_id)
+                elif time_bot.users[last_chat_id].is_updating:
+                    time_bot.add_events(last_chat_id, last_chat_text)
 
-            new_offset = last_update_id + 1
+                if last_chat_text.find("/remove_events") != -1 and not in_process:
+                    time_bot.start_removing(last_chat_id)
+                elif last_chat_text.find("/remove_events") != -1 and time_bot.users[last_chat_id].is_updating:
+                    time_bot.send_message(last_chat_id, "Сначала завершите процесс добавления")
+                elif last_chat_text.find("/stop_removing") != -1:
+                    time_bot.end_removing(last_chat_id)
+                elif time_bot.users[last_chat_id].is_removing:
+                    time_bot.remove_events(last_chat_id, last_chat_text)
 
-        time_bot.remind()
-        now = datetime.datetime.now()
-        if now.minute == 0:
-            time_bot.sync()
+                new_offset = last_update_id + 1
+
+            time_bot.remind()
+            now = datetime.datetime.now()
+            if now.minute == 23:
+                time_bot.sync()
+            if now.day == "Mon" and now.hour == 0:
+                time_bot.refresh()
+        except Exception as e:
+            print(e, "in main")
 
 
 if __name__ == '__main__':
